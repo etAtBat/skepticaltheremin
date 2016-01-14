ab629f37b8c1d0d1f92125292e745bf4ba2a1d4a
m-sandusky <msandusky08@gmail.com>
2016-01-14 11:17:44 -0800

    Enhances navbar functionality (responsive for mobile) Adds bootstrap js cdn to index.html Adds a UserStoryList view

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,8 +1,8 @@
 var React = require('react');
-var DropDownList = require('./DropDownList');
+var UserStoryList = require('./UserStoryList');
 
 var NavBar = React.createClass({
-  
+
   handleClick: function() {
     this.setState({open: !this.state.open});
 
@@ -23,7 +23,7 @@ var NavBar = React.createClass({
     return (
       <nav className="navbar navbar-default navbar-inverse navbar-fixed-top">
         <div className="container-fluid">
-          <div className="navbar-header">
+          <div className="navbar-header" >
             <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
               <span className="sr-only">Toggle navigation</span>
               <span className="icon-bar"></span>
@@ -38,13 +38,8 @@ var NavBar = React.createClass({
               <li><a href="#">+ Add a Story</a></li>
               <li className="dropdown">
                 <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">View A Story<span className="caret"></span></a>
-                <ul className="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li role="separator" className="divider"></li>
-                  <li><a href="#">Separated link</a></li>
-                </ul>
+                {/*** Adds UserStoryList Dropdown ***/}
+                <UserStoryList />
               </li>
               <li><a href='#'>Logout</a></li>
             </ul>
